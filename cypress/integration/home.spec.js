cfd1e82b2be564a520e843b13a4bef130655a8b2
natan1563 <natromafe@gmail.com>
2021-10-25 19:48:10 -0300

    Segunda fase de testes

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,9 +1,24 @@
 describe('Equipe de Quality Assurance UNIDEV (Home)', () => {
+    const baseUrl = 'http://localhost:8080';
 
     it('Deve exibir mensagem de erro ao submeter o formulário em branco', () => {
-        cy.visit("http://localhost:8080"),
+        cy.visit(baseUrl),
         cy.get('#shortner').click()
-        cy.contains('.box').should('be.visible')
-    })
-    
+        cy.get('#sadFace').should('be.visible')
+    });
+
+    it('Deve exibir mensagem de erro ao submeter o formulário com dados inválidos', () => {
+        cy.visit(baseUrl),
+        cy.get('#convertLink').type('abc123')
+        cy.get('#shortner').click()
+        cy.get('#sadFace').should('be.visible')
+    });
+
+    it('Deve exibir mensagem de erro ao submeter o formulário com dados inválidos', () => {
+        cy.visit(baseUrl),
+        cy.get('#convertLink').type('www.abc123.com')
+        cy.get('#shortner').click()
+        cy.get('.socials-network').should('be.visible')
+    });
+
 })
